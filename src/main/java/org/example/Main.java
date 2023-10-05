2f6f4053134af133534b41ca1c67ca96918b6bf3
Nikita Shaymardanov <shaymardanovnikita@gmail.com>
2023-10-05 20:27:30 +0300

    commit eight

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,21 +1,31 @@
 package org.example;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
 import java.util.List;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-
-//        for (int i = 1; i <= 99999; i++) {
-//            //System.out.println("Hello and welcome, world!"i);
-//            try {
-//                Thread.sleep(1000);
-//            } catch (InterruptedException e) {
-//                throw new RuntimeException(e);
-//            }
-//            System.out.println("i = " + i);
-//        }
-
+        System.setProperty("webdriver.chrome.driver",System.getProperty("user.dir")+"\\drivers\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+        driver.get("https://demoqa.com/text-box");
+        driver.manage().window().maximize();
+        WebElement currentElement = driver.findElement(By.xpath("//input[@id=\'userName\']"));
+        currentElement.sendKeys("Nikita");
+        currentElement = driver.findElement(By.xpath("//input[@id=\'userEmail\']"));
+        currentElement.sendKeys("shaymardanovnikita@gmail.com");
+        currentElement = driver.findElement(By.xpath("//textarea[@id=\'currentAddress\']"));
+        currentElement.sendKeys("ул. Пушкина, д. Колотушкина");
+        currentElement = driver.findElement(By.xpath("//textarea[@id=\'permanentAddress\']"));
+        currentElement.sendKeys("ул. Колотушкина, д. Пушкина");
+        currentElement = driver.findElement(By.xpath("//button[@id=\'submit\']"));
+        currentElement.click();
     }
 }
